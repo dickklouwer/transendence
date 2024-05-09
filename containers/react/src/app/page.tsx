8ba39c88ab71abbfe876f706209a078a78fd8484
Jisse Meruma <jissemeruma@gmail.com>
2024-05-09 15:42:36 +0200

    Still learining React and following tutorials

--- containers/react/src/app/page.tsx
+++ containers/react/src/app/page.tsx
@@ -1,6 +1,42 @@
+"use client"
+
 import Image from "next/image";
 
-export default function Page() {
-  return <h1>Hello, js!
-  </h1>;
+export default function HomePage() {
+  
+  const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];  
+  
+  
+  function handleClick() {
+    console.log("increment like count")
+  }
+
+
+  return ( 
+  <div>
+    <Header title="Develop. Preview. Ship." />
+    <ul>
+      {names.map((name, idx) => (
+        <li key={idx}>{name}</li>
+      ))}
+    </ul>
+    <button onClick={handleClick}>Like & Subscribe</button>
+  </div>
+  );
+}
+
+function createTitle(title : string)
+{
+  if (title) {
+    return title;
+  } else {
+    return 'Default Title';
+  }
+
+}
+
+function Header ({ title } : {title:string}) 
+{
+  console.log(title);
+  return <h1>{createTitle(title)}</h1>
 }
